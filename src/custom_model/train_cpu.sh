ec2a228ca636b1d4a7956eecf3e67e310eb8a60d
prerana <prerana.chaks@gmail.com>
2024-11-06 16:51:00 +0800

    training code custom model

---
+++ src/custom_model/train_cpu.sh
@@ -0,0 +1,3 @@
+accelerate launch --config_file "../common_utils/configs/accelerate_cpu_config.yaml" run_recolor_training.py --train_batch_size 1 --val_batch_size 1 --learning_rate 0.0002 \
+ --num_epochs 1000 --validation_interval 2 --train_data_path "../../datasets/processed_palettenet_data_sample/val/recolorization_data.json" --val_data_path "../../datasets/processed_palettenet_data_sample/val/recolorization_data.json" 
+ 
